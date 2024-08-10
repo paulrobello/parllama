b2589c70a0b31663e9efcd5f6e06fe48a67bd92f
Paul Robello <probello@gmail.com>
2024-08-09 22:12:18 -0700

    more work on rag

--- parllama/models/rag.py
+++ parllama/models/rag.py
@@ -2,11 +2,16 @@
 
 from __future__ import annotations
 
-from typing import Literal
+import abc
+import os
+from typing import Literal, Optional
 from uuid import uuid4
 
 from pydantic import BaseModel
 from pydantic import Field
+from pymilvus import MilvusClient  # type: ignore
+
+from parllama.settings_manager import settings
 
 
 class RagBase(BaseModel):
@@ -16,32 +21,85 @@ class RagBase(BaseModel):
     name: str
 
 
-VectorStoreType = Literal["Milvus"]
+StoreType = Literal["Milvus"]
+store_types: list[StoreType] = ["Milvus"]
 
-VectorStoreLocationType = Literal["Local", "URL"]
+StoreLocationType = Literal["Local", "URL"]
+store_location_types: list[StoreLocationType] = ["Local", "URL"]
 
 
-class VectorStoreBase(RagBase):
+class StoreBase(RagBase, abc.ABC):
     """Base class for vector store."""
 
-    store_type: VectorStoreType
-    location_type: VectorStoreLocationType = "Local"
+    store_type: StoreType
+    location_type: StoreLocationType = "Local"
     location: str
+    username: Optional[str] = None
+    password: Optional[str] = None
+    token: Optional[str] = None
+
+    @staticmethod
+    def get_class(class_name: str) -> type[StoreBase]:
+        """Get class based on name."""
+        for cls in StoreBase.__subclasses__():
+            if cls.__name__ == class_name:
+                return cls
+        return StoreBase  # If not found, return base
+
+    def _instantiate_store(self) -> None:
+        """Instantiate the store."""
+        raise NotImplementedError("Subclasses must implement this method")
 
 
-class VectorStoreMilvus(VectorStoreBase):
+class VectorStoreMilvus(StoreBase):
     """Base class for vector store."""
 
-    store_type: VectorStoreType = "Milvus"
+    _client: MilvusClient | None = None
+    store_type: StoreType = "Milvus"
+    location: str = "milvus_demo.db"
+    collections: list[VectorCollection]
+
+    def _instantiate_store(self) -> None:
+        """Instantiate the store."""
+        if self.location_type == "Local":
+            self._client = MilvusClient(os.path.join(settings.data_dir, self.location))
+        else:
+            self._client = MilvusClient(
+                self.location,
+                user=self.username,
+                password=self.password,
+                token=self.token,
+            )
+
+        for collection in self.collections:
+            self._client.create_collection(
+                collection_name=collection.name,
+                dimension=collection.dimension,
+                id_type="str",
+            )
+
+    @property
+    def client(self) -> MilvusClient:
+        """Get the milvus client."""
+        if self._client is None:
+            self._instantiate_store()
+        return self._client
 
 
 class CollectionBase(RagBase):
     """Base class for collection."""
 
-    store: VectorStoreBase
+    store: StoreBase
+
+
+class VectorCollection(CollectionBase):
+    """Vector collection."""
+
+    dimension: int
 
 
 DataSourceType = Literal["File", "Folder"]
+data_source_types: list[DataSourceType] = ["File", "Folder"]
 
 
 class DataSourceBase(RagBase):
@@ -66,4 +124,4 @@ class DataLink(RagBase):
     """Link between data source and store."""
 
     source: DataSourceBase
-    store: VectorStoreBase
+    store: StoreBase
--- parllama/rag_manager.py
+++ parllama/rag_manager.py
@@ -2,15 +2,43 @@
 
 from __future__ import annotations
 
+import os
+import simplejson as json
+
+from parllama.models.rag import StoreBase
 from parllama.par_event_system import ParEventSystemBase
+from parllama.settings_manager import settings
 
 
 class RagManager(ParEventSystemBase):
     """RAG manager for Par Llama."""
 
+    stores: list[StoreBase]
+
     def __init__(self):
         """Initialize the data manager."""
         super().__init__(id="rag_manager")
+        self._config_file = os.path.join(settings.data_dir, "rag_config.json")
+        self.stores = []
+        self.load()
+
+    def load(self) -> None:
+        """Load the RAG configuration."""
+        if not os.path.exists(self._config_file):
+            return
+        with open(self._config_file, "rt", encoding="utf-8") as fh:
+            config = json.load(fh)
+
+        for store_config in config["stores"]:
+            store_cls = StoreBase.get_class(store_config["type"])
+            store = store_cls(**store_config)
+            self.stores.append(store)
+
+    def save(self) -> None:
+        """Save the RAG configuration."""
+        config = {"stores": [store.model_dump() for store in self.stores]}
+        with open(self._config_file, "wt", encoding="utf-8") as fh:
+            json.dump(config, fh, indent=2)
 
 
 rag_manager: RagManager = RagManager()
