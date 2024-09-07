6d4fe6b40145f1eb44a5c4ba80aa161aad0edb08
Paul Robello <probello@gmail.com>
2024-09-07 12:29:06 -0700

    add base_url property for providers that support it refactor structure fix missing props in serialization and deserialization

--- src/parllama/llm_config.py
+++ src/parllama/llm_config.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import warnings
 from dataclasses import dataclass
 from enum import Enum
+from typing import Optional
 
 from langchain._api import LangChainDeprecationWarning
 from langchain_anthropic import ChatAnthropic
@@ -20,7 +21,7 @@ from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAI
 from langchain_openai import OpenAIEmbeddings
 
-from parllama.llm_providers import LlmProvider
+from parllama.llm_providers import LlmProvider, is_provider_api_key_set
 from parllama.par_ollama_embeddings import ParOllamaEmbeddings
 from parllama.settings_manager import settings
 
@@ -47,6 +48,7 @@ class LlmConfig:
     mode: LlmMode = LlmMode.CHAT
     temperature: float = 0.5
     streaming: bool = True
+    base_url: Optional[str] = None
 
     def to_json(self) -> dict:
         """Return dict for use with json"""
@@ -56,6 +58,8 @@ class LlmConfig:
             "model_name": self.model_name,
             "mode": self.mode,
             "temperature": self.temperature,
+            "streaming": self.streaming,
+            "base_url": self.base_url,
         }
 
     @staticmethod
@@ -73,83 +77,135 @@ class LlmConfig:
             model_name=self.model_name,
             mode=self.mode,
             temperature=self.temperature,
+            streaming=self.streaming,
+            base_url=self.base_url,
         )
+
+    def _build_ollama_llm(self) -> BaseLanguageModel | BaseChatModel | Embeddings:
+        """Build the OLLAMA LLM."""
+        if self.provider != LlmProvider.OLLAMA:
+            raise ValueError(f"LLM provider is'{self.provider}' but OLLAMA requested.")
+
+        if self.mode == LlmMode.BASE:
+            return Ollama(
+                model=self.model_name,
+                temperature=self.temperature,
+                base_url=settings.ollama_host,
+            )
+        if self.mode == LlmMode.CHAT:
+            return ChatOllama(
+                model=self.model_name,
+                temperature=self.temperature,
+                base_url=settings.ollama_host,
+            )
+        if self.mode == LlmMode.EMBEDDINGS:
+            return ParOllamaEmbeddings(
+                ollama_host=settings.ollama_host, model=self.model_name
+            )
+
+        raise ValueError(f"Invalid LLM mode '{self.mode}'")
+
+    def _build_openai_llm(self) -> BaseLanguageModel | BaseChatModel | Embeddings:
+        """Build the OPENAI LLM."""
+        if self.provider != LlmProvider.OPENAI:
+            raise ValueError(f"LLM provider is'{self.provider}' but OPENAI requested.")
+        if self.mode == LlmMode.BASE:
+            return OpenAI(
+                model=self.model_name,
+                temperature=self.temperature,
+                streaming=self.streaming,
+                base_url=self.base_url,
+            )
+        if self.mode == LlmMode.CHAT:
+            return ChatOpenAI(
+                model=self.model_name,
+                temperature=self.temperature,
+                stream_usage=True,
+                streaming=self.streaming,
+                base_url=self.base_url,
+            )
+        if self.mode == LlmMode.EMBEDDINGS:
+            return OpenAIEmbeddings(model=self.model_name)
+
+        raise ValueError(f"Invalid LLM mode '{self.mode}'")
+
+    def _build_groq_llm(self) -> BaseLanguageModel | BaseChatModel | Embeddings:
+        """Build the GROQ LLM."""
+        if self.provider != LlmProvider.GROQ:
+            raise ValueError(f"LLM provider is'{self.provider}' but GROQ requested.")
+        if self.mode == LlmMode.BASE:
+            raise ValueError(
+                f"{self.provider} provider does not support mode {self.mode}"
+            )
+        if self.mode == LlmMode.CHAT:
+            return ChatGroq(model=self.model_name, temperature=self.temperature, base_url=self.base_url)  # type: ignore
+        if self.mode == LlmMode.EMBEDDINGS:
+            raise ValueError(
+                f"{self.provider} provider does not support mode {self.mode}"
+            )
+
+        raise ValueError(f"Invalid LLM mode '{self.mode}'")
+
+    def _build_anthropic_llm(self) -> BaseLanguageModel | BaseChatModel | Embeddings:
+        """Build the ANTHROPIC LLM."""
+        if self.provider != LlmProvider.ANTHROPIC:
+            raise ValueError(
+                f"LLM provider is'{self.provider}' but ANTHROPIC requested."
+            )
+        if self.mode == LlmMode.BASE:
+            raise ValueError(
+                f"{self.provider} provider does not support mode {self.mode}"
+            )
+        if self.mode == LlmMode.CHAT:
+            return ChatAnthropic(  # pyright: ignore [reportCallIssue]
+                model=self.model_name,  # pyright: ignore [reportCallIssue]
+                temperature=self.temperature,
+                streaming=self.streaming,
+                base_url=self.base_url,
+                default_headers={"anthropic-beta": "tools-2024-05-16"},
+            )
+        if self.mode == LlmMode.EMBEDDINGS:
+            raise ValueError(
+                f"{self.provider} provider does not support mode {self.mode}"
+            )
+
+        raise ValueError(f"Invalid LLM mode '{self.mode}'")
+
+    def _build_google_llm(self) -> BaseLanguageModel | BaseChatModel | Embeddings:
+        """Build the GOOGLE LLM."""
+        if self.provider != LlmProvider.GOOGLE:
+            raise ValueError(f"LLM provider is'{self.provider}' but GOOGLE requested.")
+        if self.mode == LlmMode.BASE:
+            return GoogleGenerativeAI(
+                model=self.model_name,
+                temperature=self.temperature,
+            )
+        if self.mode == LlmMode.CHAT:
+            return ChatGoogleGenerativeAI(
+                model=self.model_name,
+                temperature=self.temperature,
+            )
+        if self.mode == LlmMode.EMBEDDINGS:
+            raise ValueError(
+                f"{self.provider} provider does not support mode {self.mode}"
+            )
+
+        raise ValueError(f"Invalid LLM mode '{self.mode}'")
 
     # pylint: disable=too-many-return-statements,too-many-branches
     def _build_llm(self) -> BaseLanguageModel | BaseChatModel | Embeddings:
         """Build the LLM."""
         if self.provider == LlmProvider.OLLAMA:
-            if self.mode == LlmMode.BASE:
-                return Ollama(
-                    model=self.model_name,
-                    temperature=self.temperature,
-                    base_url=settings.ollama_host,
-                )
-            if self.mode == LlmMode.CHAT:
-                return ChatOllama(
-                    model=self.model_name,
-                    temperature=self.temperature,
-                    base_url=settings.ollama_host,
-                )
-            if self.mode == LlmMode.EMBEDDINGS:
-                return ParOllamaEmbeddings(
-                    ollama_host=settings.ollama_host, model=self.model_name
-                )
-        elif self.provider == LlmProvider.OPENAI:
-            if self.mode == LlmMode.BASE:
-                return OpenAI(
-                    model=self.model_name,
-                    temperature=self.temperature,
-                    streaming=self.streaming,
-                )
-            if self.mode == LlmMode.CHAT:
-                return ChatOpenAI(
-                    model=self.model_name,
-                    temperature=self.temperature,
-                    stream_usage=True,
-                    streaming=self.streaming,
-                )
-            if self.mode == LlmMode.EMBEDDINGS:
-                return OpenAIEmbeddings(model=self.model_name)
-        elif self.provider == LlmProvider.GROQ:
-            if self.mode == LlmMode.BASE:
-                raise ValueError(
-                    f"{self.provider} provider does not support mode {self.mode}"
-                )
-            if self.mode == LlmMode.CHAT:
-                return ChatGroq(model=self.model_name, temperature=self.temperature)  # type: ignore
-            if self.mode == LlmMode.EMBEDDINGS:
-                raise ValueError(
-                    f"{self.provider} provider does not support mode {self.mode}"
-                )
-        elif self.provider == LlmProvider.ANTHROPIC:
-            if self.mode == LlmMode.BASE:
-                raise ValueError(
-                    f"{self.provider} provider does not support mode {self.mode}"
-                )
-            if self.mode == LlmMode.CHAT:
-                return ChatAnthropic(  # pyright: ignore [reportCallIssue]
-                    model=self.model_name,  # pyright: ignore [reportCallIssue]
-                    temperature=self.temperature,
-                    streaming=self.streaming,
-                )
-            if self.mode == LlmMode.EMBEDDINGS:
-                raise ValueError(
-                    f"{self.provider} provider does not support mode {self.mode}"
-                )
-        elif self.provider == LlmProvider.GOOGLE:
-            if self.mode == LlmMode.BASE:
-                return GoogleGenerativeAI(
-                    model=self.model_name, temperature=self.temperature
-                )
-            if self.mode == LlmMode.CHAT:
-                return ChatGoogleGenerativeAI(
-                    model=self.model_name, temperature=self.temperature
-                )
-            if self.mode == LlmMode.EMBEDDINGS:
-                raise ValueError(
-                    f"{self.provider} provider does not support mode {self.mode}"
-                )
+            return self._build_ollama_llm()
+        if self.provider == LlmProvider.OPENAI:
+            return self._build_openai_llm()
+        if self.provider == LlmProvider.GROQ:
+            return self._build_groq_llm()
+        if self.provider == LlmProvider.ANTHROPIC:
+            return self._build_anthropic_llm()
+        if self.provider == LlmProvider.GOOGLE:
+            return self._build_google_llm()
+
         raise ValueError(
             f"Invalid LLM provider '{self.provider}' or mode '{self.mode}'"
         )
@@ -174,3 +230,7 @@ class LlmConfig:
         if isinstance(llm, Embeddings):
             return llm
         raise ValueError(f"LLM mode '{self.mode}' does not support embeddings.")
+
+    def is_api_key_set(self) -> bool:
+        """Check if API key is set for the provider."""
+        return is_provider_api_key_set(self.provider)
