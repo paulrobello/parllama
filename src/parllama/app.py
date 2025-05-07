e752e33b34a69d3b5759b6121e427326a0bb2625
Alejo Grigera Sutro <23289347+artofthesmart@users.noreply.github.com>
2025-05-07 12:38:14 -0700

    Update app.py (#52)
    Fixed and clarifies the no-connection policy for this project.

--- src/parllama/app.py
+++ src/parllama/app.py
@@ -186,7 +186,7 @@ Thank your for trying ParLlama!
 Please take a moment to familiarize yourself with the the various options.
 New options are being added all the time! Check out Whats New on the repo.
 [link]https://github.com/paulrobello/parllama?tab=readme-ov-file#whats-new[/link]
-By default ParLlama makes not attempt to connect to the internet.
+By default ParLlama makes no attempt to connect to the internet and collects no data from you.
 If you would like to auto check for updates, you can enable it in the Startup section of Options.
 If you want to use other providers, you can set them up in the Providers section of the Options tab.
 Some functions are only available via slash / commands on that chat tab. You can use /help to see what commands are available.
